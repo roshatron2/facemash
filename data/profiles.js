c9ad376b6fb8c8028923dc47066902c6a1a02f42
roshatron2 <roshatron2@gmail.com>
2020-06-08 20:27:27 +0530

    Cluster DB not updating

---
+++ data/profiles.js
@@ -0,0 +1,392 @@
+[
+  {
+    rating: 1500,
+    _id: "5ed550666216ed00f0d9950d",
+    name: "Gigi Hadid",
+    image: "https://www.gstatic.com/tv/thumb/persons/742421/742421_v9_ba.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed552763137ba5578a0ea2d",
+    name: "Kendall Jenner",
+    image: "https://www.gstatic.com/tv/thumb/persons/532957/532957_v9_bb.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed552c13137ba5578a0ea2e",
+    name: "Miranda Kerr",
+    image:
+      "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fnomanazish%2Ffiles%2F2019%2F06%2F1QErdwD8-1200x1241.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed552e03137ba5578a0ea2f",
+    name: "Martha Hunt",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/ad/Martha_Hunt.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed5533f3137ba5578a0ea30",
+    name: "Candice Swanepoel",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/0/06/Candice-Swanepoel_2010-03-31_VictoriasSecretStoreChicago_photo-by-Adam_Bielawski.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed553813137ba5578a0ea31",
+    name: "Alessandra Ambrosio",
+    image: "https://pmcwwd.files.wordpress.com/2020/01/alessandra2.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed553b63137ba5578a0ea32",
+    name: "Gisele Bundchen",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMTEwMjkyMTk1MzBeQTJeQWpwZ15BbWU4MDg2MzQzMjAy._V1_QL50_SY999_CR0,0,632,999_AL_.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed553f93137ba5578a0ea33",
+    name: "Doutzen Kroes",
+    image:
+      "https://futurefornature.org/wp-content/uploads/2019/02/doutzenpff.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed554433137ba5578a0ea34",
+    name: "Romee Strijd",
+    image:
+      "https://i.pinimg.com/originals/7a/2d/5c/7a2d5c71b0a502db3706c88986f2115a.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed5548b3137ba5578a0ea35",
+    name: "Elsa Hosk",
+    image:
+      "https://i.pinimg.com/originals/e5/b9/c0/e5b9c0ced0da74dce3a6190940e20c6f.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed554c53137ba5578a0ea36",
+    name: "Stella Maxwell",
+    image:
+      "https://imagez.tmz.com/image/a0/1by1/2019/10/31/a0d7305fd5484bada8e7caf49c0cf06f_xl.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed555033137ba5578a0ea37",
+    name: "Josephine Skriver",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b2/Josephine_Skriver_%28cropped%29.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed555743137ba5578a0ea38",
+    name: "Lily Aldridge",
+    image:
+      "https://i.pinimg.com/originals/43/d3/1d/43d31d1dd3a0b10b9c976d2ef57a0c20.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed555cb3137ba5578a0ea39",
+    name: "Irina Shayk",
+    image: "https://pbs.twimg.com/media/BUOmRlDIcAAbjya.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed5562f3137ba5578a0ea3a",
+    name: "Bella Hadid",
+    image:
+      "https://i.insider.com/5ebaa85d204ad36b70154c34?width=600&format=jpeg&auto=webp",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed556903137ba5578a0ea3b",
+    name: "Hailey Rhode Bieber",
+    image:
+      "https://starsunfolded.com/wp-content/uploads/2018/07/Hailey-Baldwin-1.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed556d13137ba5578a0ea3c",
+    name: "Selena Gomez",
+    image:
+      "https://akns-images.eonline.com/eol_images/Entire_Site/2019923/rs_600x600-191023091535-600-selena-gomez-10-23.jpg?fit=around|1200:1200&crop=1200:1200;center,top&output-quality=90",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed556f63137ba5578a0ea3d",
+    name: "Taylor Swift",
+    image:
+      "https://lh3.googleusercontent.com/n7fFMNQq8yJ3dMPX64BW6Xt_nJepzHZcjm-k2XaXyM-XxF8N5ddQ1E9dAJ9hQTdi8YGdtn5NqyaregAbGp-eQJg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed557433137ba5578a0ea3e",
+    name: "Katy Perry",
+    image: "https://usupdates.com/wp-content/uploads/2019/07/katy-perry.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed557963137ba5578a0ea3f",
+    name: "Shakira",
+    image:
+      "https://www.biography.com/.image/t_share/MTE5NDg0MDU0NzExNDA0MDQ3/shakira-189151-1-402.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed557d83137ba5578a0ea40",
+    name: "Jennifer Lopez",
+    image:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/jennifer-lopez-oscars-1581351334.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed558173137ba5578a0ea41",
+    name: "Beyoncé",
+    image: "https://www.gstatic.com/tv/thumb/persons/221587/221587_v9_bc.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed558633137ba5578a0ea42",
+    name: "Kylie Jenner",
+    image: "https://www.gstatic.com/tv/thumb/persons/532961/532961_v9_bc.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed558e33137ba5578a0ea43",
+    name: "Kriti Sanon",
+    image:
+      "https://filmfare.wwmindia.com/content/2020/mar/kritisanon11584437649.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed5590e3137ba5578a0ea44",
+    name: "Disha Patani",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BZmM3YTg2ZjUtNWMwMi00NWE2LWE1YmMtYWM1ZDViNmYzOWRkXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed559593137ba5578a0ea45",
+    name: "Kiara Advani",
+    image:
+      "https://filmfare.wwmindia.com/content/2020/mar/kiaraadvani11583760480.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed5598b3137ba5578a0ea46",
+    name: "Alia Bhatt",
+    image: "https://www.gstatic.com/tv/thumb/persons/685932/685932_v9_ba.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed559e03137ba5578a0ea47",
+    name: "Deepika Padukone",
+    image:
+      "https://www.easterneye.biz/wp-content/uploads/2020/01/52047757_1025036794349900_8322494094294891884_n-1.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55a0d3137ba5578a0ea48",
+    name: "Katrina Kaif",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/65/Katrina_Kaif_in_2018.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55a4d3137ba5578a0ea49",
+    name: "Kareena Kapoor Khan",
+    image:
+      "https://filmfare.wwmindia.com/content/2020/mar/kareenakapoorkhan41583398090.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55abf3137ba5578a0ea4a",
+    name: "Aishwarya Rai Bachchan",
+    image:
+      "https://starsunfolded.com/wp-content/uploads/2014/06/Aishwarya-Rai-3-.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55b1e3137ba5578a0ea4b",
+    name: "Karisma Kapoor",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d2/KarismaKapoor02.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55b563137ba5578a0ea4c",
+    name: "Kate Upton",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMTk3MTA2ODQ1MF5BMl5BanBnXkFtZTcwMjkwMDYxNw@@._V1_.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55b863137ba5578a0ea4d",
+    name: "Alexandra Daddario",
+    image:
+      "https://i.pinimg.com/originals/3e/3a/8d/3e3a8de7c4a4af96911252e5b99e0013.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55bcb3137ba5578a0ea4e",
+    name: "Nina Agdal",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/ac/New_Era_brand_ambassador_Nina_Agdal_%2826361856771%29.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55bfa3137ba5578a0ea4f",
+    name: "Barbara Palvin",
+    image:
+      "https://i.pinimg.com/originals/49/7f/17/497f170b4be78d7597c41be4c2fad90a.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55c5d3137ba5578a0ea50",
+    name: "Samantha Hoopes",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Samantha_Hoopes_in_2014_%28cropped%29.jpg/220px-Samantha_Hoopes_in_2014_%28cropped%29.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55c863137ba5578a0ea51",
+    name: "Kate Bock",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/9/97/Kate_Bock_at_SI_Swim_City_2016.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55ce53137ba5578a0ea52",
+    name: "Hannah Ferguson",
+    image:
+      "https://i.pinimg.com/originals/a9/71/6f/a9716f4febb82e353029470cf346c562.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55d243137ba5578a0ea53",
+    name: "Rosie Huntington-Whiteley",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMTM1Nzg4Nzc2NV5BMl5BanBnXkFtZTcwODI2NDY3NQ@@._V1_.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55d573137ba5578a0ea54",
+    name: "Megan Fox",
+    image:
+      "https://pyxis.nymag.com/v1/imgs/ac5/3b7/88330ab910c3e1f0e7479378eb2c1f316e-megan-fox.rvertical.w1200.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55d793137ba5578a0ea55",
+    name: "Jessica Alba",
+    image:
+      "https://i.pinimg.com/originals/01/55/a7/0155a75a4a4ba89ea015c835063fccf8.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55dab3137ba5578a0ea56",
+    name: "Eva Mendes",
+    image:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/inarticle-eva-look3a-copy-1555014029.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55ddf3137ba5578a0ea57",
+    name: "Blake Lively",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/c/c7/Cannes_090_%2827830786580%29.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55e053137ba5578a0ea58",
+    name: "Amber Heard",
+    image: "https://www.gstatic.com/tv/thumb/persons/375633/375633_v9_bb.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55ea53137ba5578a0ea59",
+    name: "Neha Sharma",
+    image:
+      "https://i0.wp.com/www.socialnews.xyz/wp-content/uploads/2020/02/03/Actress-Neha-Sharma-Latest-Stills-.jpg?quality=90&zoom=1&ssl=1",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55edf3137ba5578a0ea5a",
+    name: "Anushka Sharma",
+    image: "https://www.gstatic.com/tv/thumb/persons/532643/532643_v9_ba.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55f523137ba5578a0ea5b",
+    name: "Priyanka Chopra",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6c/Priyanka-chopra-gesf-2018-7565.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed55fb33137ba5578a0ea5c",
+    name: "Shruti Haasan",
+    image:
+      "https://filmfare.wwmindia.com/content/2019/oct/shrutihaasan31570975585.jpg",
+    __v: 0,
+  },
+  {
+    rating: 1500,
+    _id: "5ed5600f3137ba5578a0ea5d",
+    name: "Tamannaah",
+    image:
+      "https://i2.wp.com/www.socialnews.xyz/wp-content/uploads/2019/08/20/Actress-Tamannaah-Bhatia-Glam-Stills-for-Sye-Raa-Teaser-Launch-.jpg?quality=90&zoom=1&ssl=1",
+    __v: 0,
+  },
+];
